3c6782750365dd063b831301746cd0285c5890c8
Bharat Dev Burman <bharat.singh7924@gmail.com>
2020-11-25 17:03:07 +0530

    shuang: rootdir: update makefile
    Signed-off-by: Bharat Dev Burman <bharat.singh7924@gmail.com>
    Change-Id: I0cc3887d05f0e86629c1c86c153b76964635ba24

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -29,5 +29,13 @@ LOCAL_MODULE       := init.recovery.qcom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.recovery.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.usb.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := init.qcom.usb.rc
+LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
 include $(BUILD_PREBUILT)
